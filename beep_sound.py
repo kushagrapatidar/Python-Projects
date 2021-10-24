43b9430786c378599bb134f2c9230e3cdc57528b
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-24 15:07:19 +0530

    main

--- beep_sound.py
+++ beep_sound.py
@@ -1,9 +1,11 @@
 import winsound
 
 def beep():
+    #winsound.Beep(1000//4,1000)
+    i=1000
+    while 1000>=i>=1500:#131069
+        if i%4==0:
+            print(i//4)
+            winsound.Beep(i//4,100)
+            i=i+4
 
-    [winsound.Beep(i%4,5000) for i in range(10000) if i%4==0]
-    # f=0
-    # for i in range(10000):
-    #     if i%4==0:
-    #         winsound.Beep(f+=1,5000)
