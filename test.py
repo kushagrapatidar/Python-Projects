1e1f2744f1601b5c06b1ef6b73c4194d899583ad
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-25 23:21:01 +0530

    main

--- test.py
+++ test.py
@@ -2,9 +2,9 @@
 #qr=Image.open('myqr.png')
 #qr.show()
 
-from beep_sound import beep
-beep()
+# from beep_sound import beep
+# beep()
 
-# from face_recognition import get_faces
-# get_faces()
+from face_recognition import get_faces
+get_faces()
 
